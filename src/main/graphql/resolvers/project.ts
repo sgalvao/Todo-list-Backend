96d3ababce03931ff26d41fb0ad91c616bae3674
Silvao <silvio.gabrielgt2000@hotmail.com>
2022-05-02 02:23:44 -0300

    feat: frontend hookUp

--- src/main/graphql/resolvers/project.ts
+++ src/main/graphql/resolvers/project.ts
@@ -8,21 +8,17 @@ import {
 export default {
   Query: {
     loadProjects: (_, {}, { userId }) => {
-      console.log("🚀🚀  PROJECT resolver LOAD ARGS:", userId);
       return makeLoadProject().load(userId);
     },
   },
   Mutation: {
     createProject: (_, { name }, { userId }) => {
-      console.log("🚀🚀  project resolver ARGS:", userId);
       return makeCreateProject().create({ name, userId });
     },
     deleteProject: (_, { id }, { userId }) => {
-      console.log("🚀🚀  project resolver ARGS:", id);
       return makeDeleteProject().delete(id);
     },
     updateProject: (_, { id, name }, { userId }) => {
-      console.log("🚀🚀  project resolver ARGS:", id);
       return makeUpdateProject().update(id, name);
     },
   },
--- src/main/graphql/resolvers/task.ts
+++ src/main/graphql/resolvers/task.ts
@@ -7,15 +7,12 @@ import {
 export default {
   Mutation: {
     createTask: (_, { task }, { userId }) => {
-      console.log("🚀🚀  TASK resolver CREATE ARGS:", task);
       return makeCreateTask().create({ ...task });
     },
     deleteTask: (_, { id }, { userId }) => {
-      console.log("🚀🚀  TASK resolver DELETE ARGS:", id);
       return makeDeleteTask().delete(id);
     },
     updateTaskStatus: (_, { id }, { userId }) => {
-      console.log("🚀🚀  TASK resolver DELETE ARGS:", id);
       return makeUpdateTask().update(id);
     },
   },
